2e9fb615a4328f5aa9f0873c5470adbf5155224e
tdewin <tdewin@users.noreply.github.com>
2025-09-22 13:28:39 +0200

    Update dnf-veeam-sync.py
    add log

--- capture/dnf-veeam-sync.py
+++ capture/dnf-veeam-sync.py
@@ -3,6 +3,7 @@ import subprocess
 from urllib.parse import urlparse
 import dnf
 
+from datetime import datetime
 
 base = dnf.Base()
 
@@ -10,17 +11,21 @@ base.conf.read(priority=dnf.conf.PRIO_MAINCONFIG)
 base.conf.substitutions.update_from_etc(installroot=base.conf.installroot, varsdir=base.conf.varsdir)
 base.read_all_repos()
 
-alllogs = ["Synced:"]
+alllogs = [f"{datetime.now().isoformat()} Synced:"]
 veeamrepos = {k: v for k, v in base.repos.items() if k.startswith("veeam-")}
 for repoid in veeamrepos:
   repo = base.repos[repoid]
   url = repo.baseurl[0]
   parsed = urlparse(url)
   path_only = parsed.path.lstrip("/")
-  log = f'{repoid} {url} > /mirror/{path_only}'
+  log = f'{datetime.now().isoformat()}: {repoid} {url} > /mirror/{path_only}'
   print(f"Running {log}")
   alllogs.append(log)
   torun = ["dnf","reposync","--download-meta","-p",f"/mirror/{path_only}","--norepopath","--repo",repoid]
   subprocess.run(torun)
 
-print("\n".join(alllogs))
+
+with open("/mirror/log", "a") as file:
+  file.write("\n---\n")
+  file.write("\n".join(alllogs))
+  file.write("\n")
